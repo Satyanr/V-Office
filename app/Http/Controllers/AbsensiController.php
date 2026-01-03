728421990c405a1c6f763a271ea9b32d74d117d9
Satya Dairenji <90590677+Satyanr@users.noreply.github.com>
2026-01-03 14:43:50 +0700

    required update

--- app/Http/Controllers/AbsensiController.php
+++ app/Http/Controllers/AbsensiController.php
@@ -17,7 +17,6 @@ class AbsensiController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' => 'required|string|max:255',
             'image' => 'required',
         ]);
 
